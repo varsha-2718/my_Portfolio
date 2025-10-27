499a686c6a83584d2e67fedda5b8056cc4a44f1f
Varsha Thakur <varshathakur9845@gmail.com>
2025-10-27 19:27:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let a = 5;
+let b = 3;
+console.log("a + b = ",a + b);
+console.log("a - b = ",a - b);
+console.log("a * b = ",a * b);
+console.log("a / b = ",a / b);
+console.log("a % b = ",a % b);
+console.log("a ** b = ",a ** b);
