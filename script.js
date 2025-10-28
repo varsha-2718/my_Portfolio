e08bf98dfb84e269b14b7f27f83716a1f9d711cf
Varsha Thakur <varshathakur9845@gmail.com>
2025-10-28 12:07:07 +0530

    script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-let a = 5;
-let b = 3;
-console.log("a + b = ",a + b);
-console.log("a - b = ",a - b);
-console.log("a * b = ",a * b);
-console.log("a / b = ",a / b);
-console.log("a % b = ",a % b);
-console.log("a ** b = ",a ** b);
